62b9d7a76bfcaa43b19f8a62fd135080d7781ce1
Muhammad Haziq Firdaus <m.haziq.f.9f@gmail.com>
2025-11-10 20:53:52 +0700

    change type input

--- resources/views/components/table/form.blade.php
+++ resources/views/components/table/form.blade.php
@@ -1,17 +1,17 @@
 <form>
     <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">Nama</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
         <div id="emailHelp" class="form-text">Masukkan Nama</div>
     </div>
     <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">NIM</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
         <div id="emailHelp" class="form-text">Masukkan NIM</div>
     </div>
     <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">Email</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
         <div id="emailHelp" class="form-text">Masukkan Email</div>
     </div>
     <button type="submit" class="btn btn-warning text-light font-semibold">Submit</button>
